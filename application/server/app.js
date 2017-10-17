9b8f4cabf2b2b3d2481fd48a956303bb88d797af
Wes Duff <slysop@gmail.com>
2017-10-17 16:52:22 -0600

    working app, need to check webpack build and place in express root (server)

--- application/server/app.js
+++ application/server/app.js
@@ -1,57 +1,82 @@
-const express = require('express');
-const http = require('http');
-var path = require('path');
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var compress = require('compression');
+var session = require('express-session');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var path = require('path');
+var expressValidator = require('express-validator');
+var connectAssets = require('connect-assets');
+
+/**
+ * Controllers (route handlers).
+ */
+
+var homeController = require('../controllers/home');
+
+/**
+ * API keys and Passport configuration.
+ */
+
+/*var secrets = require('./config/secrets');*/
+
+/**
+ * Create Express server.
+ */
 
 var app = express();
 
+/**
+ * Connect to MongoDB.
+
+
+mongoose.connect(secrets.db);
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+});
+
+
+/**
+ * Express configuration.
+ */
+
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'html');
-
+app.use(compress());
+app.use(connectAssets({
+  paths: [path.join(__dirname, '../assets/css'), path.join(__dirname, '../assets/js')]
+}));
+app.use('/images/', express.static(path.join(process.cwd(), '../assets/images')))
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, '../dist/assets')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../assets'), { maxAge: 31557600000 }));
 
 //assign the swig view engine to .html files
 var swig = require('swig');
 app.engine('html', swig.renderFile);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+/**
+ * Main routes.
+ */
 
-  // development error handler
-  // will print stacktrace
-  if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-    });
-  }
+app.get('/', homeController.index);
 
-  /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-  });
-  
-  // production error handler
-  // no stacktraces leaked to user
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {}
-    });
-  });
-  
-  app.listen(777);
-  
-  module.exports = app;
+
+/**
+ * Start Express server.
+ */
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+module.exports = app;
